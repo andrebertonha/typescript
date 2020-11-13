d3351301f57b6948ced4581141983b5859f1f165
andre <andrebertonhadev@gmail.com>
2020-11-13 11:51:44 -0800

    tipagem e interfaces

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
+
+// string, number, boolean, object, array
+// interfaces
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'andrebertonhadev@gmail.com',
+    password: '123456',
+    techs: [
+      'Nodejs',
+      'React',
+      'React Native',
+      { title: 'Javascript', experience: 80 },      
+    ],
+  });
+
   return response.json({ message: 'Hello World' });
 }
